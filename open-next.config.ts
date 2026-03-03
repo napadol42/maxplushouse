58e493897a5898e24f38273f169d7a690f2c3ae8
napadol42 <napadol4222@gmail.com>
2026-03-04 01:31:15 +0700

    fix: add wrangler.jsonc with correct WORKER_SELF_REFERENCE binding for Cloudflare deploy

---
+++ open-next.config.ts
@@ -0,0 +1,5 @@
+// default open-next config
+// @ts-ignore - This import will resolve during the Cloudflare build
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({});
